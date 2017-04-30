e84e89db25f18ab9c8c30cfb3ea9e7bf041574f9
Lorenzo Mori <lor.mori@gmail.com>
2017-04-30 17:30:07 +0100

    First commit - first pass on chat with simple UI in one scene.

---
+++ UnityChat/Assets/Code/PlayerChat.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class PlayerChat : NetworkBehaviour
+{
+    public InputField input = null;
+    public Text text = null;
+    public Button sendButton = null;
+
+    Color playerColor = new Color();
+
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        Debug.Log( "Server" );
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        base.OnStartLocalPlayer();
+
+        playerColor = new Color( Random.Range( 0, 256 ) / 256f, Random.Range( 0, 256 ) / 256f, Random.Range( 0, 256 ) / 256f );
+
+        text.text = "Client " + playerControllerId + " joined chat... say hello! \n";
+        text.color = playerColor;
+
+        Debug.Log( "Local Player" );
+    }
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+
+        sendButton.onClick.AddListener( OnSendButton );
+
+        Debug.Log( "Client" );
+    }
+
+    private void OnSendButton()
+    {
+        CmdSendChatMessage( input.text );
+        input.text = string.Empty;
+        input.Select();
+        input.ActivateInputField();
+    }
+
+    void Update()
+    {
+        if ( !isLocalPlayer )
+        {
+            return;
+        }
+
+        if ( Input.GetKeyDown( KeyCode.Return ) )
+        {
+            OnSendButton();
+        }
+    }
+
+    [CommandAttribute]
+    private void CmdSendChatMessage( string inMessage )
+    {
+        if ( !string.IsNullOrEmpty( inMessage ) )
+        {
+            RpcReceiveMessage( inMessage );
+        }
+
+        if ( isServer )
+        {
+            Debug.Log( "Received Message " + inMessage );
+        }
+    }
+
+    [ClientRpcAttribute]
+    private void RpcReceiveMessage( string inMessage )
+    {
+        text.text += inMessage + "\n";
+    }
+}
