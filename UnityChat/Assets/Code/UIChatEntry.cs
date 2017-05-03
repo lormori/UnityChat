a0e0e719c88f01a9e30b3b3ecbd4ff956fb03801
Lorenzo Mori <lor.mori@gmail.com>
2017-05-03 19:53:04 +0100

    Trying adding UIChatEntry and Chat prefabs

---
+++ UnityChat/Assets/Code/UIChatEntry.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIChatEntry : MonoBehaviour
+{
+    public Text chatText = null;
+    public Image backing = null;
+
+    public void ShowMessage(Color inBackingColor, string inText)
+    {
+        chatText.text = inText;
+        backing.color = inBackingColor;
+    }
+}
